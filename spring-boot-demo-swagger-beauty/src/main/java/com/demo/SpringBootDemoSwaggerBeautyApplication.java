8a1df6062fefa7c8c4d94ce194ac77f6f1443db2
peng hu <hup@hzphfin.com>
2019-10-11 18:35:03 +0800

    Initial commit

---
+++ spring-boot-demo-swagger-beauty/src/main/java/com/demo/SpringBootDemoSwaggerBeautyApplication.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动器
+ *
+ * @author Leo
+ */
+@SpringBootApplication
+public class SpringBootDemoSwaggerBeautyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoSwaggerBeautyApplication.class, args);
+    }
+}
---
+++ spring-boot-demo-swagger-beauty/src/main/java/com/demo/swagger/beauty/common/ApiResponse.java
@@ -0,0 +1,41 @@
+package com.demo.swagger.beauty.common;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName ApiResponse
+ * @Description TODO
+ * @Author Leo
+ * @Date 2019/10/11 18:22
+ * @Version 1.0
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "通用PI接口返回", description = "Common Api Response")
+public class ApiResponse<T> implements Serializable {
+    private static final long serialVersionUID = -8987146499044811408L;
+    /**
+     * 通用返回状态
+     */
+    @ApiModelProperty(value = "通用返回状态", required = true)
+    private Integer code;
+    /**
+     * 通用返回信息
+     */
+    @ApiModelProperty(value = "通用返回信息", required = true)
+    private String message;
+    /**
+     * 通用返回数据
+     */
+    @ApiModelProperty(value = "通用返回数据", required = true)
+    private T data;
+}
