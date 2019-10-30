0676c5407ea1abc8c43d2eea0fc3b6fcc42d95d1
peng hu <hup@hzphfin.com>
2019-10-30 19:14:33 +0800

    actuator demo

---
+++ spring-boot-demo-actuator/src/test/java/com/demo/actuator/SpringBootDemoActuatorApplicationTests.java
@@ -0,0 +1,23 @@
+package com.demo.actuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @ClassName SpringBootDemoActuatorApplicationTests
+ * @Description TODO
+ * @Author Leo
+ * @Date 2019/10/30 18:31
+ * @Version 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoActuatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
